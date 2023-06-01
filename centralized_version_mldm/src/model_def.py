3bedeb432a633c547e808fa50be23f13b4bb982b
root <root@cmd-0-20f56692-6953-4531-a9cc-d33d79ffc6f0.1-fitting-zebra>
2023-06-01 16:03:52 +0000

    update

--- centralized_version_mldm/src/model_def.py
+++ centralized_version_mldm/src/model_def.py
@@ -1,8 +1,8 @@
 import os, torch
 import numpy as np
 
-from typing import Any, Dict, Union, Sequence
-from determined.pytorch import DataLoader, PyTorchTrial, PyTorchTrialContext
+from typing import Any, Dict, Union, Sequence, cast, Tuple
+from determined import pytorch as det_pytorch
 
 from libauc.optimizers import PESG, Adam
 from libauc.losses import AUCM_MultiLabel, CrossEntropyLoss
@@ -27,34 +27,34 @@ def set_computation_precision(matmul32=False, cudnn32=True):
     torch.backends.cuda.matmul.allow_tf32 = matmul32
     torch.backends.cudnn.allow_tf32 = cudnn32
     
-class MyTrial(PyTorchTrial):
-    def __init__(self, context: PyTorchTrialContext) -> None:
+class LungDiseaseTrial(det_pytorch.PyTorchTrial):
+    def __init__(self, context: det_pytorch.yTorchTrialContext) -> None:
         self.context = context
         self.rank_number = self.context.distributed.get_rank()
         #self.download_directory = (f"/tmp/data-rank{self.context.distributed.get_rank()}"
          
         # Set Hyperparameters
-        seed = 123
-        model_checkpoint_dir = './model_checkpoint'
-        amp_mode = False
-        batch_size = 32
-        lr = 0.001                     # using smaller learning rate is better
-        weight_decay = 1e-5
-        #alpha = []                    # a list of imbalance ratio. To mitigate the imbalance data for each binary class (number positive samples divided by number negative samples)
-        data_root = './CheXpert-v1.0-small/'
+        self.seed = self.context.get_hparam("seed")
+        self.amp_mode = self.context.get_hparam("amp_mode")
+        self.batch_size = self.context.get_hparam("batch_size")
+        self.lr = self.context.get_hparam("lr")                     # using smaller learning rate is better
+        self.weight_decay = self.context.get_hparam("weight_decay")  
+        self.data_root = self.context.get_hparam("data_root")
+        
+        if self.amp_mode:
+            pass
+            #self.context.configure_apex_amp() # Use this to configure the mixed precision training of the model for distributed training in MLDE cluster 
         
         # Enable/disable reproducibility and some library optimization options
         #print(os.environ)
-        set_all_seeds(seed, deterministic=True, benchmark=True)
-        set_computation_precision(matmul32=False, cudnn32=True)
+        #set_all_seeds(seed, deterministic=True, benchmark=True)
+        #set_computation_precision(matmul32=False, cudnn32=True)
 
         # Define dataset
         print('Create dataloader ...')
         train_cols=['Cardiomegaly', 'Edema', 'Consolidation', 'Atelectasis',  'Pleural Effusion']
-        traindSet   = CheXpert(csv_path=data_root+'train.csv', image_root_path=data_root, use_upsampling=True, use_frontal=True, image_size=320, mode='train', class_index=-1)
-        testSet     =  CheXpert(csv_path=data_root+'valid.csv',  image_root_path=data_root, use_upsampling=True, use_frontal=True, image_size=320, mode='valid', class_index=-1)
-        trainloader =  torch.utils.data.DataLoader(traindSet, batch_size=batch_size, num_workers=2, drop_last=True, shuffle=True)
-        testloader  =  torch.utils.data.DataLoader(testSet, batch_size=batch_size, num_workers=2, drop_last=False, shuffle=False)
+        self.traindSet   = CheXpert(csv_path=self.data_root+'train.csv', image_root_path=self.data_root, use_upsampling=True, use_frontal=True, image_size=320, mode='train', class_index=-1)
+        self.valSet     =  CheXpert(csv_path=self.data_root+'valid.csv',  image_root_path=self.data_root, use_upsampling=True, use_frontal=True, image_size=320, mode='valid', class_index=-1)
         print('Create dataloader done.')
         print()
 
@@ -65,15 +65,15 @@ class MyTrial(PyTorchTrial):
         
         # Define cost function
         print("Create loss function ...")
-        temp_targets = np.array(traindSet.df.loc[:, traindSet.select_cols]).astype(dtype=np.int64).copy()
+        temp_targets = np.array(self.traindSet.df.loc[:, self.traindSet.select_cols]).astype(dtype=np.int64).copy()
         class_weights = calculate_multilabel_binary_class_weight(temp_targets)
-        loss_fn = Custom_MultiLabel_AlphaBalanced_FocalLoss(alpha=class_weights, gamma=2) 
+        self.loss_fn = Custom_MultiLabel_AlphaBalanced_FocalLoss(alpha=class_weights, gamma=2) 
         #loss_fn = AUCM_MultiLabel(imratio=traindSet.imratio_list, num_classes=5)
         print("Create loss function done")
 
         # Define optimizer
         print('Create optimizer ...')
-        if isinstance(loss_fn, AUCM_MultiLabel) == True: 
+        if isinstance(self.loss_fn, AUCM_MultiLabel) == True: 
             pass
             # optimizer = PESG(model_wrapper.model, 
             #                 a=loss_fn.a, 
@@ -85,7 +85,7 @@ class MyTrial(PyTorchTrial):
             #                 weight_decay=weight_decay, 
             #                 device='cuda')
         else:
-            optimizer = Adam(model_wrapper.model, lr=lr, weight_decay=weight_decay)
+            optimizer = Adam(model_wrapper.model, lr=self.lr, weight_decay=self.weight_decay)
         print('Create optimizer done.')
 
         # wrap things up
@@ -93,28 +93,108 @@ class MyTrial(PyTorchTrial):
         self.optimizer =  self.context.wrap_optimizer(optimizer)
 
     
+    def build_training_data_loader(self) -> det_pytorch.DataLoader:
+        return det_pytorch.DataLoader(self.traindSet, batch_size=self.context.get_per_slot_batch_size(), drop_last=True, shuffle=True)
 
-    def build_training_data_loader(self) -> DataLoader:
-        return DataLoader()
 
-    def build_validation_data_loader(self) -> DataLoader:
-        return DataLoader()
+    def build_validation_data_loader(self) -> det_pytorch.DataLoader:
+        return det_pytorch.DataLoader(self.valSet, batch_size=self.context.get_per_slot_batch_size(), drop_last=False, shuffle=False)
+
 
     def train_batch(self, batch: TorchData, epoch_idx: int, batch_idx: int)  -> Dict[str, Any]:
-        return {}
+        
+        loss = None
+        
+        # if self.amp_mode == True:
+        #     with autocast():
+        #         y_pred = self.model(train_data)
+        #         if isinstance(self.loss_fn, AUCM_MultiLabel) == True:
+        #             y_pred = torch.sigmoid(y_pred)
+        #         loss = self.loss_fn(y_pred, train_labels)
+
+        #     scaler.scale(loss).backward()
+        #     scaler.step(optimizer)
+        #     scaler.update()
+        #     optimizer.zero_grad()  
+        # else:
+        #     y_pred = self.model(train_data)
+        #     if isinstance(self.loss_fn, AUCM_MultiLabel) == True:
+        #         y_pred = torch.sigmoid(y_pred)
+        #     loss = self.loss_fn(y_pred, train_labels)
+        #     loss.backward()
+        #     optimizer.step()
+        #     optimizer.zero_grad()   
+        # train_metrics['loss'].append(float(loss))
+        
+        # fetch data
+        batch = cast(Tuple[torch.Tensor, torch.Tensor], batch)
+        data, labels = batch
+
+        # forward pass and compute loss
+        output_logits = self.model(data)
+        loss = self.loss_fn(output_logits, labels)
+
+        # backward pass
+        self.context.backward(loss)
+        self.context.step_optimizer(self.optimizer)
+           
+        return {"train_loss": loss}
+
 
     def evaluate_batch(self, batch: TorchData) -> Dict[str, Any]:
-        return {}
+        
+        accuracy = None
+        
+        # # Evaluate model performance metrics
+        # model_wrapper.model.eval()
+        # with torch.no_grad():    
+        #     test_pred = []
+        #     test_true = [] 
+        #     for jdx, data in enumerate(testloader):
+        #         test_data, test_labels = data
+        #         test_data = test_data.cuda()
+        #         y_pred = model_wrapper.model(test_data)
+        #         y_pred = torch.sigmoid(y_pred)
+        #         test_pred.append(y_pred.cpu().detach().numpy())
+        #         test_true.append(test_labels.numpy())
+        #     test_true = np.concatenate(test_true)
+        #     test_pred = np.concatenate(test_pred)
+        #     val_auc_mean =  roc_auc_score(test_true, test_pred, average="macro") 
+        #     val_auc_mean_micro =  roc_auc_score(test_true, test_pred, average="micro") 
+        #     val_auc_class =  roc_auc_score(test_true, test_pred, average=None) 
+        #     model_wrapper.model.train()
+        # val_metrics['val_auc_mean'].append([float(val_auc_mean)])
+        # val_metrics['val_auc_mean_micro'].append([float(val_auc_mean_micro)])
+        # val_metrics['val_auc_class'].append(val_auc_class)
+        # val_metrics['best_val_auc'].append([float(best_val_auc)])
+
+        batch = cast(Tuple[torch.Tensor, torch.Tensor], batch)
+        data, labels = batch
+        output_logits = self.model(data)
+        y_pred = torch.sigmoid(output_logits)
+        loss = self.loss_fn(output_logits, labels)
+
+        # pred = output_logits.argmax(dim=1, keepdim=True)
+        # accuracy = pred.eq(labels.view_as(pred)).sum().item() / len(data)
+        
+        return {"val_loss": loss}
+    
+    # def predict(
+    #     self, X: np.ndarray, names, meta
+    # ) -> Union[np.ndarray, List, str, bytes, Dict]:
+    #     image = Image.fromarray(X.astype(np.uint8))
+    #     logging.info(f"Image size : {image.size}")
+
+    #     image = self.get_test_transforms()(image)
+    #     image = image.unsqueeze(0)
+
+    #     with torch.no_grad():
+    #         output = self.model(image)[0]
+    #         pred = np.argmax(output)
+    #         logging.info(f"Prediction is : {pred}")
+
+    #     return [self.labels[pred]]
+
+
     
     
-    
-    # # Create dataloader
-    # print('Create dataloader ...')
-    # data_root = './CheXpert-v1.0-small/'
-    # train_cols=['Cardiomegaly', 'Edema', 'Consolidation', 'Atelectasis',  'Pleural Effusion']
-    # traindSet   = CheXpert(csv_path=data_root+'train.csv', image_root_path=data_root, use_upsampling=True, use_frontal=True, image_size=320, mode='train', class_index=-1)
-    # testSet     =  CheXpert(csv_path=data_root+'valid.csv',  image_root_path=data_root, use_upsampling=True, use_frontal=True, image_size=320, mode='valid', class_index=-1)
-    # trainloader =  torch.utils.data.DataLoader(traindSet, batch_size=batch_size, num_workers=2, drop_last=True, shuffle=True)
-    # testloader  =  torch.utils.data.DataLoader(testSet, batch_size=batch_size, num_workers=2, drop_last=False, shuffle=False)
-    # print('Create dataloader done.')
-    # print()
